16b9b5dc3682c8b72ca86db2f02642ac5227c197
varaprasadp <vara199715@gmail.com>
2024-02-04 14:11:26 +0530

    Initial commit

---
+++ src/main/java/com/personal/hmsgateway/HmsGatewayApplication.java
@@ -0,0 +1,13 @@
+package com.personal.hmsgateway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HmsGatewayApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HmsGatewayApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/personal/hmsgateway/HmsGatewayApplicationTests.java
@@ -0,0 +1,13 @@
+package com.personal.hmsgateway;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HmsGatewayApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
